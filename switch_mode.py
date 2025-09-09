52d3e83a9ec5216bb178517ecbf6cc56b86434d1
GracieSk13 <gsketoe@cub.uca.edu>
2025-09-09 13:50:50 -0500

    Update switch_mode.py

--- switch_mode.py
+++ switch_mode.py
@@ -1,6 +1,43 @@
-from machine import Pin, PWM, Timer
+from machine import Pin, PWM
 from time import sleep
 
-# SETUP
+#instead of import machine ->
+# we make machine now jus Pin
 
-# LOOP
+
+#SETUP
+mode = "On" 
+dimmer = PWM(Pin(15))
+dimmer.freq(1000)
+
+#led = Pin(15, Pin.OUT)
+button = Pin(14, Pin.IN, Pin. PULL_DOWN)
+    
+def toggle_led(pin):
+    global mode
+    if(mode == "On"):
+       mode = "Fade"
+    elif(mode == "Fade"): # else if in python
+        mode = "On"
+    
+    #dimmer.duty_u16(65535)
+    #print(f"button status level: {dimmer.duty_u16()}")
+    #print(led.value())
+    
+button.irq(trigger=Pin.IRQ_FALLING, handler=toggle_led)    
+    
+#LOOP
+
+while True:
+    if (mode == "On"):
+        dimmer.duty_u16(65535) #has light at max brightness
+        sleep(1)               #nighty night
+    elif(mode == "Fade"):
+        for duty in range(65535):
+            dimmer.duty_u16(duty)
+            sleep(0.0001)
+
+        for duty in range(65535, 2, -1):
+            dimmer.duty_u16(duty)
+            sleep(0.0001)
+
